a0094fb41417bad060ff897ac46a30e076fdd71a
Lewis-mbui <lewmugo14479@gmail.com>
2025-10-10 07:52:22 +0300

    small change to buttons toggle exercise

--- exercises/06-DOM-CSS/03-buttons-toggle.js
+++ exercises/06-DOM-CSS/03-buttons-toggle.js
@@ -1,7 +1,16 @@
 function toggleButton(buttonClass) {
   const buttonElement = document.querySelector(buttonClass);
 
-  if (buttonElement.classList.contains("is-toggled"))
-    buttonElement.classList.remove("is-toggled");
-  else buttonElement.classList.add("is-toggled");
+  if (!buttonElement.classList.contains("is-toggled")) {
+    turnOffPreviousButton();
+    buttonElement.classList.add("is-toggled");
+  }
+  else buttonElement.classList.remove("is-toggled");
+}
+
+function turnOffPreviousButton() {
+  const toggledButton = document.querySelector('.is-toggled');
+
+  if (toggledButton)
+    toggledButton.classList.remove('is-toggled');
 }
